9cb768a6efc40146555fa8f3839fa8d3ad897fda
腾来 <tenglai@fulan.com.cn>
2020-12-01 12:51:42 +0800

    create blog

---
+++ js/pagediary.js
@@ -0,0 +1,7 @@
+layui.use(["jquery", "flow"], function () {
+  var $ = layui.jquery;
+  $(".yearToggle").click(function () {
+    $(this).parent("h2").siblings(".timeline-month").slideToggle("slow");
+    $(this).siblings("i").toggleClass("fa-caret-down fa-caret-right");
+  });
+});
